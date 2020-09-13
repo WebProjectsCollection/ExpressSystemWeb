2ddaebfbf609bc60d57f61325cf99af4910775bf
Spencer Yang <Spencer_Yang@Jabil.com>
2020-09-13 15:25:47 +0800

    update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('UniformMS app is running!');
+    expect(page.getTitleText()).toEqual('ExpressSystemWeb app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'UniformMS'`, () => {
+  it(`should have as title 'ExpressSystemWeb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('UniformMS');
+    expect(app.title).toEqual('ExpressSystemWeb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('UniformMS app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ExpressSystemWeb app is running!');
   });
 });
